bd2ccf856711bdf601dc013e3ede89f18727ca99
Riyadh Uddin <noreplythnx@gmail.com>
2023-04-22 18:31:01 +0600

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='To automate the extraction and analysis of key information from Ministry of Foreign Affairs mission documents, improving efficiency, accuracy, and scalability.',
+    author='Riyadh Uddin',
+    license='MIT',
+)
